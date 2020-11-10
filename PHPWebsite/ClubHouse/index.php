af6a4cea2627f2844978bf78cb9eb5b46e10bb49
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2020-11-10 20:05:39 +0200

    Fixed include files

--- PHPWebsite/ClubHouse/index.php
+++ PHPWebsite/ClubHouse/index.php
@@ -5,10 +5,11 @@ $page_title = "Club House"
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php include($_SERVER["DOCUMENT_ROOT"] . "/Components/Navigation/index.php") ?>
 		<?php include($_SERVER["DOCUMENT_ROOT"] . "/Common/Header.php") ?>
 	</head>
 	<body>
+		<?php include($_SERVER["DOCUMENT_ROOT"] . "/Components/Navigation/index.php") ?>
 		<?php include($_SERVER["DOCUMENT_ROOT"] . "/Components/WebsiteHeader/index.php") ?>
+		<?php include($_SERVER["DOCUMENT_ROOT"] . "/Components/WebsiteFooter/index.php") ?>
 	</body>
 </html>
---
+++ PHPWebsite/Components/WebsiteFooter/index.php
@@ -0,0 +1,23 @@
+<!-- Website footer styles -->
+<link rel="stylesheet" href="/Components/WebsiteFooter/Styles.css">
+<div class="website-footer background-color-default-accent">
+	<div class="footer-background"></div>
+	<div class="footer-sections">	
+		<div class="section-item">
+			<h3>Site Map</h3>
+			<ul>
+				<li>Home</li>
+				<li>Gallery</li>
+			</ul>
+		</div>
+		<div class="section-item">
+
+		</div>
+		<div class="section-item">
+
+		</div>
+	</div>
+	<div class="website-license">
+
+	</div>
+</div>
