7ee459e23634b3c368bbff9ff44d6e117e6d8643
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2020-11-10 19:39:22 +0200

    Completed the header component

--- PHPWebsite/Components/Websiteheader/index.php
+++ PHPWebsite/Components/Websiteheader/index.php
@@ -6,11 +6,12 @@
 		<source src="/Resources/backgrounds/header-video.webm" type="video/webm"></source>
 	</video>
 	<div class="header-content component">
-		<h1>Welcome</h1>
-		<h2>To The Fore Horsemen</h2>
+		<h2>Welcome To</h2>
+		<h1>The Fore Horsemen</h1>
 		<p>
-			We are Golf addicts, amd we are proud of it.<br/>
+			We are Golf addicts, and we are proud of it.<br/>
 			"Hier laaik almal Golf."
 		</p>
 	</div>
+	<span class="scroll-icon"></span>
 </div>
