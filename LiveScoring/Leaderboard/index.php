1fe96cd412dc136ff6a128d797da309bd8ab738d
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2021-04-15 15:23:58 +0200

    FIxed css link

--- LiveScoring/Leaderboard/index.php
+++ LiveScoring/Leaderboard/index.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="./LeaderBoard/css/styles.css?t=<?php echo $siteUpdated?>">
+<link rel="stylesheet" href="./Leaderboard/css/styles.css?t=<?php echo $siteUpdated?>">
 <script type="text/javascript" src="./Leaderboard/js/jquery-leaderboard.js?t=<?php echo $siteUpdated?>"></script>
 <script type="text/javascript" src="./Leaderboard/js/app.js?t=<?php echo $siteUpdated?>"></script>
 
