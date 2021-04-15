15504246dfb0676a198d99e180e938e35a64cdac
Wickus <vandermerwewickus@gmail.com>
2021-04-15 20:55:31 +0200

    UX Fix

--- LiveScoring/EnterScore/js/jquery-score-form.js
+++ LiveScoring/EnterScore/js/jquery-score-form.js
@@ -62,6 +62,7 @@ class ScoreForm {
         $(this.element).append(`
 			<div class="score-form">
 				<form name="score">
+					<p>S.I: ${self.coarseData.holes[self.userInfo.hole + 1].stroke} / Par:${self.coarseData.holes[self.userInfo.hole + 1].par}</p>
 					<label>Enter score:</label>
 					<input type="number" name="strokes" placeholder="Enter score" value="${self.userInfo.scorecard[parseInt(self.userInfo.hole) - 1].strokes}">
 					<label>Enter putts:</label>
